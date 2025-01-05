3f414285b60979754b69d9d8beae9bcaf275f6f7
xwisniax96 <xwisniax96@gmail.com>
2025-01-05 21:10:09 +0100

    Dodałem zmiany w AppDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using RoomBooking.Models;
 
 namespace RoomBooking.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
     }
 }
