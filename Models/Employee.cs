0d34f6673e256647bdd61342afb093cdea6533e8
xwisniax96 <xwisniax96@gmail.com>
2025-01-10 00:03:52 +0100

    kod

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,13 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RoomBooking.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Imię jest wymagane.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Imię nie może zawierać znaków specjalnych ani cyfr.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email jest wymagany.")]
+        [EmailAddress(ErrorMessage = "Podaj poprawny adres email.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon jest wymagany.")]
+        [RegularExpression(@"^\+?[0-9]{9,15}$", ErrorMessage = "Podaj poprawny numer telefonu z kierunkowym (np. +48123456789).")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Stanowisko jest wymagane.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Stanowisko nie może zawierać znaków specjalnych ani cyfr.")]
         public string WorkPosition { get; set; }
         public int HotelId { get; set; }
         [ForeignKey("HotelId")]
--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -1,10 +1,20 @@
-﻿namespace RoomBooking.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomBooking.Models
 {
     public class Hotel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Imię jest wymagane.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Imię nie może zawierać znaków specjalnych.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Opis jest wymagany.")]
+        [MaxLength(1000, ErrorMessage = "Opis nie może być dłuższy niż 1000 znaków.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Adres jest wymagany.")]
+        [MaxLength(200, ErrorMessage = "Adres nie może być dłuższy niż 200 znaków.")]
         public string Address { get; set; }
         public List<Room> Rooms { get; set; } = new();
         public List<Employee> Employees { get; set; } = new();
